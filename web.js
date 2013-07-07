855d2564c2ce70e0be8ce91e47a20f1ff1110531
Marryat <marryat@procorva.com>
2013-07-07 07:16:32 +0000

    make file load synchronous

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var indexBuffer = fs.readFile('./index.html');
+  var indexBuffer = fs.readFileSync('./index.html');
   
 
   response.send(indexBuffer.toString);
